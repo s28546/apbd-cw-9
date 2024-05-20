955aeccc6aae5b650f87739d45bd6092013c297d
Hubert Michalski <s28546@pjwstk.edu.pl>
2024-05-20 13:09:09 +0200

    'asd'

---
+++ WebApplication1/WebApplication1/Controllers/Controller.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication1.Controllers;
+
+public class Controller
+{
+    
+}
+
+/*
+        w kontrollerze, i ze to z contextu entities
+        
+        List<Book> books = await context.Books.
+        .Include(x => x.IdAuthorNavigation) -> inner join 
+        .ToListAsync(cancellationToken)
+        
+        
+        
+        
+                
+*/
